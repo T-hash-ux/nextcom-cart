c365519d79731065d0118e6b3bd5921ddd797587
Tyrrance <tyrrancem37@gmail.com>
2023-05-17 20:24:19 -0400

    Set up the code for the main application component which is the App js.

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -17,7 +17,7 @@ export default function AddClientModal() {
     </div>      
     </button>
 
-<div className="modal fade" id="addClientModal" tabindex="-1" aria-labelledby="addClientModal" aria-hidden="true">
+<div className="modal fade" id="addClientModal" aria-labelledby="addClientModal" aria-hidden="true">
   <div className="modal-dialog">
     <div className="modal-content">
       <div className="modal-header">
@@ -25,18 +25,26 @@ export default function AddClientModal() {
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div className="modal-body">
-        ...
+        <form onSubmit={onSubmit}>
+          <div className="mb-3">
+            <label className="form-label">Name</label>
+            <input type="name" className="form-control"  id="name" value={name} onChange={ (e) => setName (e.target.value)} />
+
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Email</label>
+            <input type="email" className="form-control" id="email" value={email} onChange={ (e) => setEmail (e.target.value)} />
+
+          </div>
+          <button type="submit" data-bs-dismiss="modal" className="btn btn-secondary">Submit</button>
+        </form>
       </div>
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
 
         </div>
     </div>
   </div>
-</div>
-
-
-        </>
-    )
+</>
+);
 
 }
 
